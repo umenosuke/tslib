cfe943cefa7059246bff4164b3b3cc9763f99bb4
umenosuke <umenosuke.net@gmail.com>
2020-04-03 21:14:56 +0900

    ルーティングテーブルの追加と削除を実装

--- src/network/RoutingTable.ts
+++ src/network/RoutingTable.ts
@@ -1,4 +1,5 @@
 import { Route } from "./Route.js";
+import { IP } from "./IP.js";
 
 export { RoutingTable };
 
@@ -60,6 +61,54 @@ class RoutingTable<T extends { equal: (compVal: T) => boolean, toString: () => s
         }
 
         return false;
+    }
+
+    public searchRoute(ip: IP): Route<T>[] {
+        if (this.route.contain(ip)) {
+            for (let i = 0, len = this.subTree.length; i < len; i++) {
+                if (this.subTree[i].route.contain(ip)) {
+                    return this.subTree[i].searchRoute(ip);
+                }
+            }
+
+            const res: Route<T>[] = [];
+            res.push(this.route);
+            for (let i = 0, len = this.redundantRotue.length; i < len; i++) {
+                res.push(this.redundantRotue[i]);
+            }
+            return res;
+        }
+
+        return [];
+    }
+
+    public removeRoute(route: Route<T>): RoutingTable<T>[] {
+        if (this.route.same(route)) {
+            if (this.route.equal(route)) {
+                if (this.redundantRotue.length > 0) {
+                    this.route = this.redundantRotue.splice(0, 1)[0];
+                    return [this];
+                }
+
+                return this.subTree;
+            }
+
+            for (let i = 0, len = this.redundantRotue.length; i < len; i++) {
+                if (this.redundantRotue[i].equal(route)) {
+                    this.redundantRotue.splice(i, 1);
+                    break;
+                }
+            }
+            return [this];
+        }
+
+        for (let i = 0, len = this.subTree.length; i < len; i++) {
+            const res = this.subTree.splice(i, 1)[0].removeRoute(route);;
+            for (let j = 0, len = res.length; j < len; j++) {
+                this.subTree.push(res[j]);
+            }
+        }
+        return [this];
     }
 
     public show(): void {
