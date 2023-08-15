290e238a63233b90d0dfbaf9694125920349675c
umenosuke <umenosuke.net@gmail.com>
2023-08-15 18:57:11 +0900

    querySelector のNull排除したやつのやつ

---
+++ src/html/searchUtil.ts
@@ -0,0 +1,13 @@
+export { search, searchAll }
+
+function search<T extends HTMLElement>(query: string, elem: Document | Element = document): T {
+    const e = elem.querySelector<T>(query);
+    if (e == null) { throw new Error("not found : \"" + query + "\""); }
+    return e;
+};
+
+function searchAll<T extends HTMLElement>(query: string, elem: Document | Element = document): NodeListOf<T> {
+    const e = elem.querySelectorAll<T>(query);
+    if (e == null) { throw new Error("not found : \"" + query + "\""); }
+    return e;
+};
