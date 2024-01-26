43f2d45ee54b5db566515de76913688aeb18d9b8
umenosuke <umenosuke.net@gmail.com>
2024-01-26 23:59:11 +0900

    ライブラリをそのままのディレクトリ構成でいけるように

--- .script/_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export _PRJ_NAME='tslib'
-export COMPOSE_PROJECT_NAME="${_PRJ_NAME}"
-
-export _GIT_TAG="$(git describe --tags --abbrev=0)"
-export _GIT_HASH="$(git rev-parse --short HEAD)"
-
-export _USER="$(id -u):$(id -g)" 
--- .script/sass_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname $(readlink -f $0))/../"
-_BASE_DIR="$(pwd)"
-
-_SRC_PATH="${1}"
-_OUTPUT_PATH="${2}"
-
-sass --update --style compressed "${_SRC_PATH}:${_OUTPUT_PATH}"
