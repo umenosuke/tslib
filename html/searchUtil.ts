1c51c329970c82dae761e6ae6f7e1e98105fca84
umenosuke <umenosuke.net@gmail.com>
2024-02-20 00:28:31 +0900

    コードフォーマットの調整

--- html/searchUtil.ts
+++ html/searchUtil.ts
@@ -4,10 +4,10 @@ function search<T extends HTMLElement>(query: string, elem: Document | Element =
     const e = elem.querySelector<T>(query);
     if (e == null) { throw new Error("not found : \"" + query + "\""); }
     return e;
-};
+}
 
 function searchAll<T extends HTMLElement>(query: string, elem: Document | Element = document): NodeListOf<T> {
     const e = elem.querySelectorAll<T>(query);
     if (e.length === 0) { throw new Error("not found : \"" + query + "\""); }
     return e;
-};
+}
