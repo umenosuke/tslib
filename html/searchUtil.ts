e1c9599acc386624bda6c8377eeba8291e986f5d
umenosuke <umenosuke.net@gmail.com>
2024-02-24 19:51:18 +0900

    searchをDocumentFragmentも受けいれるようにした

--- html/searchUtil.ts
+++ html/searchUtil.ts
@@ -1,12 +1,12 @@
 export { search, searchAll }
 
-function search<T extends HTMLElement>(query: string, elem: Document | Element = document): T {
+function search<T extends HTMLElement>(query: string, elem: Document | Element | DocumentFragment = document): T {
     const e = elem.querySelector<T>(query);
     if (e == null) { throw new Error("not found : \"" + query + "\""); }
     return e;
 }
 
-function searchAll<T extends HTMLElement>(query: string, elem: Document | Element = document): NodeListOf<T> {
+function searchAll<T extends HTMLElement>(query: string, elem: Document | Element | DocumentFragment = document): NodeListOf<T> {
     const e = elem.querySelectorAll<T>(query);
     if (e.length === 0) { throw new Error("not found : \"" + query + "\""); }
     return e;
