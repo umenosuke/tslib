97f04aed8c98f4fcf8a88bbd192ddc59a594818f
umenosuke <umenosuke.net@gmail.com>
2025-05-13 13:02:35 +0900

    filepathを追加

--- filepath/extractExtension.ts
+++ filepath/extractExtension.ts
@@ -1,11 +1,15 @@
-export { extractExtension };
+export { extractExtension, extractExtensionURL };
 
-function extractExtension(path: string): string {
+function extractExtension(path: string): string | undefined {
     const m = path.match(/(\.[^\.\/]+)$/);
 
-    if (m == undefined || m[1] == undefined) {
-        return "";
+    if (m == null || m[1] == undefined) {
+        return undefined;
     }
 
     return m[1];
+}
+
+function extractExtensionURL(url: URL): string | undefined {
+    return extractExtension(url.pathname);
 }
--- filepath/extractFilename.ts
+++ filepath/extractFilename.ts
@@ -1,11 +1,14 @@
-export { extractFilename };
+export { extractFilename, extractFilenameURL };
 
-function extractFilename(path: string): string {
-    const m = path.match(/\/([^\/]+)$/);
+function extractFilename(path: string): string | undefined {
+    const m = path.match(/\/?([^\/]+)$/);
 
-    if (m == undefined || m[1] == undefined) {
-        return "";
+    if (m == null || m[1] == undefined) {
+        return undefined;
     }
 
     return m[1];
+}
+function extractFilenameURL(url: URL): string | undefined {
+    return extractFilename(url.pathname);
 }
