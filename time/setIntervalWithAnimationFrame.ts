9d7c2503dc2a0a026e6acab42e32121c5d35931c
umenosuke <umenosuke.net@gmail.com>
2025-05-14 11:30:11 +0900

    clearIntervalWithAnimationFrameを追加

--- time/setIntervalWithAnimationFrame.ts
+++ time/setIntervalWithAnimationFrame.ts
@@ -1,12 +1,20 @@
 import { TokenBucket } from "./TokenBucket.js";
 
-export { setIntervalWithAnimationFrame };
+export { setIntervalWithAnimationFrame, clearIntervalWithAnimationFrame };
 
-function setIntervalWithAnimationFrame(handler: Function, timeout: number) {
+const runningIDs = new Set<ReturnType<typeof crypto.randomUUID>>();
+
+function setIntervalWithAnimationFrame(handler: Function, timeout: number): ReturnType<typeof crypto.randomUUID> {
+    const id = crypto.randomUUID();
     const bucket = new TokenBucket(timeout, 1000);
+
+    runningIDs.add(id);
 
     const fnc = async () => {
         await bucket.tryConsumeWait(timeout);
+        if (!runningIDs.has(id)) {
+            return;
+        }
 
         try {
             await handler();
@@ -18,4 +26,14 @@ function setIntervalWithAnimationFrame(handler: Function, timeout: number) {
     }
 
     fnc();
+
+    return id;
+};
+
+function clearIntervalWithAnimationFrame(id: ReturnType<typeof crypto.randomUUID> | undefined | null): void {
+    if (id == undefined) {
+        return;
+    }
+
+    runningIDs.delete(id);
 };
